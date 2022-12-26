eee84b96a6d077f6979ad98fd42c3fcb61fffc2a
Юлия Алексеева <115483581+Julyapulya@users.noreply.github.com>
2022-12-27 01:17:17 +0700

    Update lab9.js

--- lab9.js
+++ lab9.js
@@ -1,38 +1,63 @@
 function clickMe(button) {
     if(button.innerHTML == 'Щелчок')
-    button.innerHTML = 'Спасибо';
+        button.innerHTML = 'Спасибо';
     else
-    button.innerHTML = 'Щелчок';
+        button.innerHTML = 'Щелчок';
 }
-function clickMe1(button) {
-    if(button.innerHTML == 'Юлия')
-    button.innerHTML = 'Алексеева';
-    else
-    button.innerHTML = 'Юлия';
-}
+
 function clickMe2(button) {
-    if(button.innerHTML == 'Арюна')
-    button.innerHTML = 'Шаглаева';
+    if(button.innerHTML == 'Копылов')
+        button.innerHTML = 'Кирилл';
     else
-    button.innerHTML = 'Арюна';
+        button.innerHTML = 'Копылов';
 }
-function clickMe3(button) {
-    if(button.innerHTML == 'Нажми меня')
-    button.innerHTML = 'Ещё раз нажми';
+
+function clickMe3(button)
+{ 
+    if(button.innerHTML == 'Нажми меня') 
+        button.innerHTML = 'Ещё раз нажми';
+    else if (button.innerHTML == 'Ещё раз нажми')
+        button.innerHTML = 'Ещё';
+        else if (button.innerHTML == 'Ещё')
+        button.innerHTML = 'Начать сначала';
     else
-    if(button.innerHTML == 'Ещё раз нажми')
-    button.innerHTML = 'Ещё';
-    else
-    if(button.innerHTML == 'Ещё')
-    button.innerHTML = 'Начать сначала';
-    else
-    if(button.innerHTML == 'Начать сначала')
-    button.innerHTML = 'Нажми меня';
+        button.innerHTML = 'Нажми меня';
 }
-function showCoords (event) {
-    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+})
+function showFocus(event) {
+    event.target.style.color = 'blue';
 }
 window.addEventListener('DOMContentLoaded', function() {
-let coords = document.querySelector('#coords');
-coords.addEventListener('mousemove', showCoords);
-}) ;
+    let texts = document.querySelectorAll('input[type="text"]');
+    texts[0].addEventListener('focus', showFocus);
+    texts[1].addEventListener('focus', showFocus);
+    texts[2].addEventListener('focus', showFocus);
+});
+function showBlur(event) {
+    event.target.style.color = 'black';
+} 
+window.addEventListener('DOMContentLoaded', function() {
+let texts = document.querySelectorAll('input[type="text"]'); 
+texts[0].addEventListener('blur', showBlur);
+texts[1].addEventListener('blur', showBlur);
+texts[2].addEventListener('blur', showBlur);
+});
+
+function showOutput(event){
+    document.getElementById('output').innerHTML = event.target.value;
+}
+window.addEventListener('DOMContentLoaded', function() {
+let texts = document.querySelectorAll('input[type="text"]');
+    texts[0].addEventListener('change', showOutput);
+    texts[1].addEventListener('change', showOutput);
+    texts[2].addEventListener('change', showOutput);
+});
